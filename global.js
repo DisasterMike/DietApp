51aa2c442aba282af2005ddcf669f7e8c8660505
Michael Stump <maikerustump@gmail.com>
2025-03-02 14:11:52 +0900

    basic server set up

---
+++ global.js
@@ -0,0 +1,42 @@
+import dayjs from "dayjs";
+
+const getDatetime = () => dayjs().format('YYYY-MM-DD HH:mm:ss')
+
+// const getDate = () => dayjs().format('YYYY-MM-DD')
+
+
+global.LOG = (...args) => {
+
+    const stack = new Error().stack;
+    const coreParts = stack.split('\n')[2].split(' ').filter(i => i !== '').splice(1) // [func, (path/to/file:15:5)]
+
+    let func
+    let file
+    if (coreParts.length > 1) { // called inside a function
+        func = coreParts[0]
+        file = coreParts[1].replace(/[\(\)]/g, '').split('DietApp/')[1]
+    } else {
+        func = 'anonymous'
+        file = coreParts[0].replace(/[\(\)]/g, '').split('DietApp/')[1]
+    }
+
+    console.log('LOG', getDatetime(), file, func, ...args)
+}
+
+global.ERR = (...args) => {
+    const stack = new Error().stack;
+    const coreParts = stack.split('\n')[2].split(' ').filter(i => i !== '').splice(1) // [func, (path/to/file:15:5)]
+
+    let func
+    let file
+    if (coreParts.length > 1) { // called inside a function
+        func = coreParts[0]
+        file = coreParts[1].replace(/[\(\)]/g, '').split('DietApp/')[1]
+    } else {
+        func = 'anonymous'
+        file = coreParts[0].replace(/[\(\)]/g, '').split('DietApp/')[1]
+    }
+
+    console.log('ERR', getDatetime(), file, func, ...args)
+}
+
--- index.js
+++ index.js
@@ -1,18 +1,100 @@
 import http from 'node:http';
 import fs from 'node:fs';
+import path from 'path';
+// import { dirname } from 'node:path';
+
+import { URL } from 'url';
+
+import './global.js'
 
 const host = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('hello');
 
-    if (req.url==='/') {
-        
+    let parsedUrl = new URL(req.url, 'http://localhost:3000')
+    const pathname = parsedUrl.pathname
+    // LOG(parsedUrl)
+
+    // server static files
+    const fileExt = path.extname(pathname)
+    if (fileExt) {
+        staticFiles(req, res, fileExt)
+        return
     }
-})
+
+    // serve other requests
+    switch (pathname) {
+        case '/':
+            homePage(req, res)
+            break
+        case '/login':
+            loginPage(req, res)
+            break
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' })
+		    res.end('404 not found')
+            // TODO add 404 page
+    }
+});
 
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-})
+    LOG(`Server is running on http://${host}:${port}`);
+});
+
+process
+	.on('unhandledRejection', (reason, p) => {
+		ERR('process.unhandledRejection', reason, p)
+	})
+	.on('uncaughtException', err => {
+		ERR('process.uncaughtException', err)
+	})
+
+const homePage = async (req, res) => {
+    if (req.method==='GET') {
+        serveFile('index.html', 'text/html', res)
+    }
+}
+
+const loginPage = async (req, res) => {
+    if (req.method==='GET') {
+        serveFile('pages/login.html', 'text/html', res)
+    }
+    if (req.method==='POST') {
+        // ...
+        LOG('post time...')
+        serveFile('pages/login.html', 'text/html', res)
+    }
+}
+
+const staticFiles = (req, res, fileExt) => {
+    const filePath = path.join('public', req.url);
+
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/javascript',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.ico': 'image/x-icon',
+    };
+    const contentType = mimeTypes[fileExt]
+
+    serveFile(filePath, contentType, res)
+}
+
+const serveFile = (filePath, contentType, res) => {
+
+    // LOG('filePath to serve: ', filePath)
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end(500, {error: 'Not found'});
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    })
+}
